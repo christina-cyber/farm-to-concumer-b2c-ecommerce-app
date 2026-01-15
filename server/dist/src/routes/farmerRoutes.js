9a05b6112b5f2ef01e7cae938b92ee8d55706381
christina-cyber <krishtinakhatiwada60@gmail.com>
2026-01-15 16:38:49 +0545

    fix: add gitignore and remove node_modules

--- server/dist/src/routes/farmerRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const farmerController_1 = require("../controllers/farmerController");
-const authMiddleware_1 = require("../middlewares/authMiddleware");
-const router = (0, express_1.Router)();
-router.get('/stats', authMiddleware_1.authenticateToken, (0, authMiddleware_1.authorizeRole)(['FARMER']), farmerController_1.getDashboardStats);
-exports.default = router;
